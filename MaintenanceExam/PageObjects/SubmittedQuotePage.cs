47ac015f8c248b27bbdecb7ef67c314843ac493a
JKHaugen <JHaugen@ssfcu.org>
2024-09-09 12:24:01 -0500

    Updated the test to handle the new Interest: Home checkbox and the Language Preference dropdown. Added an Assert to confirm the confirmation message appears after submitting the quote. Renamed the test class and method name to reflect changes made to the test.

---
+++ MaintenanceExam/PageObjects/SubmittedQuotePage.cs
@@ -0,0 +1,27 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace MaintenanceExam.PageObjects
+{
+    public class SubmittedQuotePage : BasePageLocal
+    {
+        private readonly By _thankYouForSubmissionLocator = By.XPath("//h3[contains(text(),'submission')]");
+
+        public SubmittedQuotePage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        /// <summary>
+        /// Grabs the thank you text after the submission of an insurance quote.
+        /// </summary>
+        /// <param name="timeout">Seconds of time to wait for submission to complete</param>
+        /// <returns>String thanking the user for the submission</returns>
+        public string GetSubmissionConfirmationMessage(int timeout)
+        {
+            WebDriverWait wait = new WebDriverWait(Driver, TimeSpan.FromSeconds(timeout));
+            wait.Until(ExpectedConditions.ElementExists(_thankYouForSubmissionLocator));
+            return GetText(_thankYouForSubmissionLocator);
+        }
+    }
+}
---
+++ MaintenanceExam/TestCases/InsuranceQuote/RequestAutoAndHomePersonalInsuranceQuoteInEnglish.cs
@@ -0,0 +1,30 @@
+﻿using MaintenanceExam.PageObjects;
+using NUnit.Framework;
+
+namespace MaintenanceExam.TestCases.InsuranceQuote
+{
+    class RequestAutoAndHomePersonalInsuranceQuoteInEnglish : BaseTestLocal
+    {
+        [Test]
+        [Category("Insurance Quote")]
+        public void RequestAutoAndHomePersonalInsuranceQuoteInEnglishCase()
+        {
+            // hack to get to the page faster
+            Driver.Value!.Url = "https://www-qa.mybranchqa.org/insurance/personal/request-a-quote";
+
+            // Update the test case to incorporate the new requirements and pass.
+            // DONE: The insurance quote page has been updated to include Interest: Home and Language Preference,
+            // the default language preference is "English".
+            bool autoInterest = true;
+            bool homeInterest = true;
+            bool otherInterest = false;
+            new GetAQuotePage(Driver.Value!).CreateRequest(autoInterest, homeInterest, otherInterest);
+
+            // DONE: Add an assert in the test to validate the confirmation message after submission.
+            string expectedConfirmation = "Thank you for your submission!";
+            int timeoutInSeconds = 2;
+            string actualConfirmation = new SubmittedQuotePage(Driver.Value!).GetSubmissionConfirmationMessage(timeoutInSeconds);
+            Assert.AreEqual(expectedConfirmation, actualConfirmation, "Verify confirmation of submission of quote");
+        }
+    }
+}
